5c5508baddd18bf30d4ed4f67bc23c940654a0ac
sizlackin <129177750+sizlackin@users.noreply.github.com>
2026-09-24 13:59:17 -0400

    Add contained-mining installer

---
+++ ActuallyUsefulTurtles/enable-contained-mining.lua
@@ -0,0 +1,33 @@
+-- Installs contained/fair mining behavior.
+-- Run on the MAIN Actually Useful Turtles controller at the normal CraftOS > prompt.
+-- Turtles will no longer x-ray/chase remembered ore veins or carve ore branches.
+
+local url = "https://raw.githubusercontent.com/sizlackin/CCTweaked-Projects/main/ActuallyUsefulTurtles/patches/classMiner.lua"
+
+if turtle then
+    error("Run this on the main Actually Useful Turtles controller.", 0)
+end
+if not fs.isDir("turtle") then
+    error("Could not find the controller turtle source folder.", 0)
+end
+
+local response, err = http.get(url)
+if not response then error("Download failed: " .. tostring(err), 0) end
+local code = response.getResponseCode()
+if code ~= 200 then
+    response.close()
+    error("Download failed: HTTP " .. tostring(code), 0)
+end
+
+local data = response.readAll()
+response.close()
+if not data or #data < 10000 then
+    error("Downloaded classMiner patch looks incomplete.", 0)
+end
+
+local f = assert(fs.open("turtle/classMiner.lua","w"))
+f.write(data)
+f.close()
+
+print("Contained mining installed.")
+print("Reboot controller, then reboot DTX-001 and DTX-002.")
